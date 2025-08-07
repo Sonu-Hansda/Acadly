703e29714baaf86c72e896d58a8ce91acda41c11
Anoop Kumar Burnwal <anoopkumar19062006@gmail.com>
2025-08-07 11:54:29 +0530

    Added basic file structure for backend and made userShema Model

---
+++ backend/models/user.js
@@ -0,0 +1,45 @@
+import {hash} from 'bcrypt';
+import mongoose , {model , Schema} from 'mongoose';
+
+const userShema = new Schema({
+    name: {
+        type: String,
+        required: true,   
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    username:{
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+        select: false, // Exclude password from queries by default
+    },
+    profilePicture: {
+        public_id:{
+            type: String,
+            required: true,
+        },
+        url:{
+            type: String,
+            required: true,
+        }
+    },
+    
+},{timestamps: true});
+
+userShema.pre("save" , async function(next){
+
+    if(!this.isModified("password")) return next();
+
+    this.password = await hash(this.password , 10);
+    
+})
+
+export const User = model('User', userShema);
