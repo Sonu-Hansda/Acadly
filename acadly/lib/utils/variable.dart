81adddb4e20246a175f39b0c9b8b0bd55df31b54
Yashi-Piparsaniya <aditiguptachp20@gmail.com>
2025-08-15 19:36:22 +0530

    Add variable.dart

---
+++ acadly/lib/utils/variable.dart
@@ -0,0 +1,21 @@
+final notes = [
+  {
+    'title' : 'Mechanics'
+  },
+  {
+    'title' : 'Maths'
+  }
+];
+
+final person = [
+  {
+    'photoURL' : 'assets/img.png',
+    'Username' : 'Yashi',
+    'Regno' : '2024UGCM024',
+    'branch' : 'ECM'
+  },
+];
+
+final dates= [
+  'Today' , 'Yesterday', 'Last Week', 'Last Month'
+];
