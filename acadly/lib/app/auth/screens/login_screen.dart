452eb63faf56c4fc2e7a042f466c99807c144c26
Yashi-Piparsaniya <aditiguptachp20@gmail.com>
2025-08-20 01:01:24 +0530

    Login done

--- acadly/lib/app/auth/screens/login_screen.dart
+++ acadly/lib/app/auth/screens/login_screen.dart
@@ -1,3 +1,4 @@
+import 'package:acadly/services/api_service.dart';
 import 'package:flutter/material.dart';
 import 'package:acadly/app/common/theme/colors.dart';
 
@@ -9,6 +10,12 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+
+  bool isLoading = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -28,23 +35,94 @@ class _LoginScreenState extends State<LoginScreen> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             TextFormField(
+              controller: emailController,
               decoration: const InputDecoration(labelText: 'Email'),
             ),
             const SizedBox(height: 16),
             TextFormField(
+              controller: passwordController,
               obscureText: true,
               decoration: const InputDecoration(labelText: 'Password'),
             ),
             Row(
               children: [
                 Expanded(child: Text(''),),
-                TextButton(onPressed: (){}, child: Text('Forgot Password?', style: Theme.of(context).textTheme.bodyMedium,)),
+                TextButton(
+                    onPressed: (){},
+                    child: Text('Forgot Password?', style: Theme.of(context).textTheme.bodyMedium,)),
               ],
             ),
             const SizedBox(height: 24),
             ElevatedButton(
-              onPressed: () {
-                Navigator.pushReplacementNamed(context, '/home');
+              onPressed: isLoading? null :()async{
+                setState(() {
+                  isLoading=true;
+                });
+                if (emailController.text.isEmpty) {
+                  setState(() {
+                    isLoading = false;
+                  });
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    const SnackBar(
+                      content: Text("Please enter your email address"),
+                      backgroundColor: AppColors.primary,
+                      behavior: SnackBarBehavior.floating,
+                    ),
+                  );
+                  return;
+                }
+
+                if (passwordController.text.isEmpty) {
+                  setState(() {
+                    isLoading = false;
+                  });
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    const SnackBar(
+                      content: Text("Please enter your password"),
+                      backgroundColor: AppColors.primary,
+                      behavior: SnackBarBehavior.floating,
+                    ),
+                  );
+                  return;
+                }
+                try{
+                  final res = await ApiService.login(emailController.text.trim(), passwordController.text.trim());
+                  if( res['success']== true){
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      const SnackBar(
+                        content: Text("Login Successful"),
+                        backgroundColor: AppColors.primary,
+                        behavior: SnackBarBehavior.floating,
+                      ),
+                    );
+                    Navigator.pushReplacementNamed(context, '/home');
+                  }
+                  else{
+                    ScaffoldMessenger.of(context).showSnackBar(
+                        SnackBar(content: Text(res['message'] ?? "Invalid email or password !" ),
+                          backgroundColor: AppColors.primary,
+                          behavior: SnackBarBehavior.floating,),
+                    );
+                  }
+                } catch(e){
+                  String errorMsg = "Something went wrong. Please try again.";
+                  if (e.toString().contains("SocketException")) {
+                    errorMsg = "No Internet connection. Please check your network.";
+                  } else if (e.toString().contains("Timeout")) {
+                    errorMsg = "Server is taking too long. Try again later.";
+                  }
+
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    SnackBar(
+                      content: Text(errorMsg),
+                      backgroundColor: AppColors.primary,
+                      behavior: SnackBarBehavior.floating,
+                    ),
+                  );
+                }
+                setState(() {
+                  isLoading = false;
+                });
               },
               style: ElevatedButton.styleFrom(
                 backgroundColor: AppColors.primary,
---
+++ acadly/lib/services/api_service.dart
@@ -0,0 +1,23 @@
+import "dart:convert";
+import 'package:http/http.dart' as http;
+
+class ApiService{
+  static const String baseUrl = "http://localhost:3000";
+
+  static Future<Map<String , dynamic>> login(String email, String password) async{
+    final url = Uri.parse("$baseUrl/api/user/login");
+    final response = await http.post(
+      url,
+      headers: {"Content-Type": "application/json"},
+      body: jsonEncode({
+        "email": email,
+        "password": password,
+      }),
+    );
+    if (response.statusCode == 200) {
+      return jsonDecode(response.body); // success
+    } else {
+      throw Exception("Login failed: ${response.body}");
+    }
+  }
+}
