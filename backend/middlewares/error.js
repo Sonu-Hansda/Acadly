54fabc0545138429f5ed4d045f40dd4da60ed0a7
Anoop Kumar Burnwal <anoopkumar19062006@gmail.com>
2025-08-21 23:22:52 +0530

    errors now comes in json format

--- backend/middlewares/error.js
+++ backend/middlewares/error.js
@@ -1,3 +1,25 @@
+const errorMiddleware = (err, req, res, next) => {
+    err.message ||= "Internal Server Error";
+    err.statusCode ||= 500;
+
+    if (err.code === 11000) {
+        const error = Object.keys(err.keyPattern).join(",");
+        err.message = `Duplicate field - ${error}`;
+        err.statusCode = 400;
+    }
+
+    if (err.name === "CastError") {
+        const errorPath = err.path;
+        err.message = `Invalid Format of ${errorPath}`;
+        err.statusCode = 400;
+    }
+
+    return res.status(err.statusCode).json({
+        success: false,
+        message: err.message,
+    });
+};
+
 const TryCatch = (passedFunction) => async(req , res , next) => {
     try {
         await passedFunction(req , res , next);
@@ -6,4 +28,4 @@ const TryCatch = (passedFunction) => async(req , res , next) => {
     }
 }
 
-export { TryCatch};
+export { errorMiddleware, TryCatch };
